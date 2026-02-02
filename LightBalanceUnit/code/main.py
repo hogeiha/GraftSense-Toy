87fbc461d68e042ec81cf596beb1701993956d40
hogeiha <1098875044@QQ.COM>
2026-02-02 17:17:55 +0800

    feat(GraftSense-Toy): 上传第一批功能稳定代码

--- LightBalanceUnit/code/main.py
+++ LightBalanceUnit/code/main.py
@@ -7,8 +7,10 @@
 
 # ======================================== 导入相关模块 =========================================
 
+from machine import Pin, ADC, I2C
 from gl5516 import GL5516
 import time
+from pcf8575 import PCF8575
 
 # ======================================== 全局变量 =============================================
 
@@ -18,10 +20,39 @@ import time
 def display_level(level: int) -> None:
     """
         根据 level 值点亮前 N 个 LED。
+        
     """
+    num=(1 << (level + 1)) - 1
+    low_8bit = num & 0xFF  # 提取低8位（原始后8位）
+    high_8bit = (num >> 8) & 0xFF  # 提取高8位（原始前8位）
+    
+    # 3. 互换位置：低8位左移8位（变高8位） + 高8位（变低8位）
+    num_swapped = (low_8bit << 8) | high_8bit
+    pcf8575.port = num_swapped
 # ======================================== 自定义类 =============================================
 
 # ======================================== 初始化配置 ===========================================
+
+i2c: I2C = I2C(id=1, sda=Pin(10), scl=Pin(11), freq=400000)
+
+# 开始扫描I2C总线上的设备，返回从机地址的列表
+devices_list: list[int] = i2c.scan()
+print('START I2C SCANNER')
+
+# 若devices_list为空，则没有设备连接到I2C总线上
+if len(devices_list) == 0:
+    print("No i2c device !")
+# 若非空，则打印从机设备地址
+else:
+    print('i2c devices found:', len(devices_list))
+    # 遍历从机设备地址列表
+    for device in devices_list:
+        print("I2C hexadecimal address: ", hex(device))
+        # 假设第一个找到的设备是PCF8575
+        PCF8575_ADDRESS = device
+
+# 创建PCF8575类实例
+pcf8575 = PCF8575(i2c, PCF8575_ADDRESS)
 
 # 初始化 GL5516 光强度传感器，连接到 GPIO26 引脚
 adc = GL5516(26)
@@ -43,5 +74,6 @@ while True:
     print("Light Intensity - Voltage: {} V, ADC Value: {}".format(voltage, adc_value))
     # 获取校准后的光强百分比
     light_level = int(adc.get_calibrated_light()*0.16)
+    display_level(light_level)
     print("Calibrated Light Level: {}".format(light_level))
-    time.sleep(2)
+    time.sleep_ms(50)
